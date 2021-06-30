f9f6a6c3c3656729c2e6c34962474bae68ea2e21
Annélise Rowan <Annelise.Rowan@gmail.com>
2021-06-29 20:54:03 -0400

    Update product-routes.js

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -18,7 +18,9 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-  Product.findOne({
+  Product.findOne({where: {
+    id: req.params.id
+  },
     include: [Category, { model: Tag, through: ProductTag }]
   }).then((productById) => {
     res.json(productById);
